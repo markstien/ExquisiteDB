a0ce067f7b6660b941688948f2ef50f45dd998f0
markstien <markstien@outlook.com>
2019-11-26 12:42:22 +0800

    完成

--- README.md
+++ README.md
@@ -1,72 +1,9 @@
-![avatar](http://39.96.84.96/ExquisiteDB/images/Alogo.png)
+![avatar](images/Alogo.png)
 # ExquisiteDB
 ========
 ### 对indexDB的简易封装
 #### Why?
-####The original indexDB API  is too hard to use.
-####原生indexDB API太难用了。
+#### The original indexDB API  is too hard to use.
+#### 原生indexDB API太难用了。
 eg:
 https://marksteinsong.github.io/ExquisiteDB/
-```html
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>ExquisiteDB--example</title>
-    <script src="dist/ExquisiteDB.js"></script>
-</head>
-<body>
-<table>
-    <thead>
-    <th>id</th>
-    <th>姓名-name</th>
-    <th>年龄-age</th>
-    </thead>
-    <tbody id="tbody">
-
-    </tbody>
-</table>
-<script>
-    let dataConstruct={
-        keyPath:"id",
-        index:[
-            {name:"name",unique:false},
-            {name:"age",unique:false}
-        ]
-    };
-    let example=new ExquisiteDB("dataExquisite",1,dataConstruct);
-    example.addOne({id:"123",name:"Mark",age:"21"},function (result) {
-        console.log(result);
-    });
-    example.getAll(function (datas) {
-        let tbody=document.getElementById("tbody");
-        datas.forEach(function (data) {
-            tbody.innerHTML+=`<tr>
-                                    <td>${data.id}</td>
-                                    <td>${data.name}</td>
-                                    <td>${data.age}</td>
-                               </tr>`;
-        });
-    });
-</script>
-</body>
-</html>
-```
-### properties:
-#### Object.addOne(data,callback)
-
-- `data`: ```<object>```
-- `callback`:```<function>```
-#### Object.getAll(callback)
-- `callback`:```<function>```
-#### Object.getOneByIndex(key,value,callback)
-
-- `key`: ```<string>```
-- `value`:```<string>```
-- `callback`:```<function>```
-
-For example:
-```exampleObject.getOneByIndex("id","123",function(result){console.log(result)})```
-
-
-未完待续...
--- dist/ExquisiteDB.js
+++ dist/ExquisiteDB.js
@@ -1,152 +1,183 @@
-/**
- *author:Markstien
- * updateTime:2019-5-21 18:45
- * License：BSD
- */
-// In the following line, you should include the prefixes of implementations you want to test.
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-// DON'T use "var indexedDB = ..." if you're not in a function.
-// Moreover, you may need references to some window.IDB* objects:
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
-if (!window.indexedDB) {
-    alert("你的浏览器不支持indexDB。");
-}else {
-    console.log("You browser supports indexDB.");
-}
-/***
- * function-return:type1(fail)||type2(success)
- * getAll:string||data
- * getOneByIndex:undefined||object
- * addOne:string||success
- * removeOne:string||success
- * changeOne:string||success
- * 数据结构
- * {
- * ketPath:"id",
- * index:[
- *     {name:"index1",unique:true},
- *     {name:"index1",unique:false},
- *     ....
- *     ....
- * ]
- * }
- * */
 const ExquisiteDB=function (databaseName,version,dataConstruct) {
-    if((typeof databaseName)!=="string"){
-        console.log("ExquisiteDB:数据库名必须是string。");
-        return 0;
-    }
+    //不对外开放的接口
+    const isSupported=function () {return !window.indexedDB?false:true;};
+    const init=function () {
+        window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+        window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+        window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+    };
+    const isDSValid=function(o){
+        return o.hasOwnProperty("keyPath")&&o.index.length>0;
+    };
+
+    if(!isDSValid(dataConstruct)){console.log("数据结构无效");return}
+    if(!isSupported()){console.log("你的浏览器不支持indexDB!");return}
+    if((typeof databaseName)!=="string"){console.log("ExquisiteDB:数据库名必须是string。");return;}
+
+    //indexDB变量初始化
+    init();
+
     this.databaseName=databaseName;
     this.version=version;
     this.request = window.indexedDB.open(this.databaseName,this.version);
+    //本地不存在数据库，创建数据库
     this.request.onupgradeneeded=function (event) {
-        console.log("创建新结构。");
-        let db = event.target.result;
+        //console.log("创建新结构。");
+        const db = event.target.result;
         //keyPath主键
-        let  objectStore = db.createObjectStore("ExquisiteDB", { keyPath: dataConstruct.keyPath });
+        const  objectStore = db.createObjectStore("ExquisiteDB", { keyPath: dataConstruct.keyPath });
         dataConstruct.index.forEach(function (index) {
             objectStore.createIndex(index.name, index.name, { unique: index.unique });
-            console.log(index);
+            //console.log(index);
         });
         // 使用事务的 oncomplete 事件确保在插入数据前对象仓库已经创建完毕
         objectStore.transaction.oncomplete = function(event) {
-           console.log(event.type);
+            console.log("数据库创建：",event.type);
         };
     }
 };
 ExquisiteDB.prototype={
     constructor:ExquisiteDB,
-    getAll:function (callback) {
-        let request=window.indexedDB.open(this.databaseName,this.version);
-        request.onsuccess=function (event) {
-            let db=event.target.result;
-            let  objectStore = db.transaction("ExquisiteDB").objectStore("ExquisiteDB");
-            objectStore.getAll().onsuccess = function(event) {
-               callback(event.target.result);
-            };
-        };
-        request.onerror=function (event) {
-            callback("打开数据库失败。");
-        }
-    },
-    getOneByIndex:function(key,value,callback){
-        let request=window.indexedDB.open(this.databaseName,this.version);
-        request.onsuccess=function (event) {
-            let db=event.target.result;
-            let  objectStore = db.transaction("ExquisiteDB").objectStore("ExquisiteDB");
-            let  index = objectStore.index(key);
-            index.get(value).onsuccess=function (event) {
-                callback(event.target.result);
-            }
-        };
-        request.onerror=function (event) {
-            callback("打开数据库失败。");
-        }
-    },
-    addOne:function (data,callback) {
-        let request=window.indexedDB.open(this.databaseName,this.version);
-        request.onsuccess=function (event) {
-            let db=event.target.result;
-            let  objectStore = db.transaction("ExquisiteDB", "readwrite").objectStore("ExquisiteDB");
-            let request=objectStore.add(data);
+    //开启事务
+    entry:function(){
+        return new Promise(resolve => {
+            const request=window.indexedDB.open(this.databaseName,this.version);
             request.onsuccess=function (event) {
-                callback(event.type);
-                // callback(event.target.result === data.ssn);
+                const db=event.target.result;
+                const  objectStore = db.transaction(["ExquisiteDB"], "readwrite").objectStore("ExquisiteDB");
+                resolve({result:true,objectStore});
             };
             request.onerror=function (event) {
-                callback(`写入失败:${event.target.error.message}`);
+                resolve({result:false,message:"打开数据库失败"});
             }
-        };
-        request.onerror=function (event) {
-            callback("打开数据库失败。");
-        }
+        })
     },
-    removeOneByKeyPath:function (keyPath,callback) {
-        let request=window.indexedDB.open(this.databaseName,this.version);
-        request.onsuccess=function (event) {
-            let db=event.target.result;
-            let request = db.transaction(["ExquisiteDB"], "readwrite")
-                .objectStore("ExquisiteDB")
-                .delete(keyPath);
-            request.onsuccess=function(event){
-                callback(event.type);
+    /**读取*/
+    //读取所有
+    getAll:function () {
+        return new Promise(resolve => {
+            const request=window.indexedDB.open(this.databaseName,this.version);
+            request.onsuccess=function (event) {
+                const db=event.target.result;
+                const  objectStore = db.transaction("ExquisiteDB").objectStore("ExquisiteDB");
+                objectStore.getAll().onsuccess = function(event) {
+                    resolve({data:event.target.result,result:true});
+                };
             };
             request.onerror=function (event) {
-                callback(`删除失败:${event.target.error.message}`);
+                resolve({result:false,message:"打开数据库失败"});
             }
-        };
-        request.onerror=function (event) {
-            callback("打开数据库失败。");
-        }
+        });
     },
-    changeOneByKeyPath:function (keyPath,dataChanged,callback) {
-        let request=window.indexedDB.open(this.databaseName,this.version);
-        request.onsuccess=function (event) {
-            let db=event.target.result;
-            let  objectStore = db.transaction(["ExquisiteDB"], "readwrite").objectStore("ExquisiteDB");
-            let request = objectStore.get(keyPath);
-            request.onerror = function(event) {
-               callback(`读取失败:${event.target.error.message}`);
-            };
-            request.onsuccess = function(event) {
-                // 获取我们想要更新的数据
-                let data = event.target.result;
-                // 更新你想修改的数据
-                data = dataChanged;
-                // 把更新过的对象放回数据库
-                let  requestUpdate = objectStore.put(data);
-                requestUpdate.onerror = function(event) {
-                    callback(`更改失败:${event.target.error.message}`);
+    //根据根据键值读取  ex.  "name","mark"
+    getItemByIndex:function(index,value){
+        return new Promise(resolve => {
+            this.entry().then(result=>{
+                const  request = result.objectStore.index(index);
+                request.get(value).onsuccess=function (event) {
+                    resolve({data:event.target.result,result:true});
+                }
+            });
+        })
+    },
+    getItemsByIndex:function(index,value){
+        return new Promise(resolve => {
+            this.entry().then(result=>{
+                const data=[];
+                const  request = result.objectStore.index(index);
+                const singleKeyRange = IDBKeyRange.only(value);
+                request.openCursor(singleKeyRange).onsuccess=function () {
+                    const cursor = event.target.result;
+                    if (cursor) {
+                        data.push(cursor.value);
+                        cursor.continue();
+                    }
+                    else {
+                        resolve({data,result:true});
+                    }
                 };
-                requestUpdate.onsuccess = function(event) {
-                    callback(event.type);
+            })
+        })
+    },
+    //根据主键读取
+    getItemByKeyPath:function(keyPath){
+        return new Promise(resolve => {
+            this.entry().then(result=>{
+                const request = result.objectStore.get(keyPath);
+                request.onsuccess=function (event) {
+                    const data = event.target.result;
+                    data?resolve({data:data,result:true}):resolve({data:[],result:false});
+                }
+            })
+        })
+    },
+    //写
+    add:function(data){
+        return new Promise(resolve => {
+            this.entry().then(result=>{
+                const request=result.objectStore.add(data);
+                request.onsuccess=function (event) {
+                    resolve({result:event.type==="success"});
                 };
-            };
-        };
-        request.onerror=function (event) {
-            callback("打开数据库失败。");
-        }
-    }
+                request.onerror=function (event) {
+                    resolve({message:`写入失败:${event.target.error.message}`,result:false});
+                }
+            })
+        })
+    },
+    //删
+    removeByKeyPath:function(keyPath){
+        return new Promise(resolve => {
+            this.entry().then(result=>{
+                const request=result.objectStore.delete(keyPath);
+                request.onsuccess=function(event){
+                    resolve({result:event.type==="success"});
+                };
+                request.onerror=function (event) {
+                    resolve({message:`删除失败:${event.target.error.message}`,result:false});
+                }
+            })
+        });
+    },
+    //改
+    changeByKeyPath:function (keyPath,dataChanged) {
+        return new Promise(resolve => {
+            this.entry().then(result=>{
+                const request = result.objectStore.get(keyPath);
+                request.onerror = function(event) {
+                    resolve({data:`读取失败:${event.target.error.message}`,result:false});
+                };
+                request.onsuccess = function(event) {
+                    // 获取想要更新的数据
+                    const oldData = event.target.result;
+                    if(!oldData){resolve({result:false,message:"keyPath不存在"});return ;}
+                    //  oldData 与  dataChanged  对比更改 有且不同的、没有的
+                    const generate=function (chang,old) {
+                        const ck=Object.keys(chang);
+                        ck.map(function (key) {
+                            //有且不同的、没有的->添加到old
+                            !old.hasOwnProperty(key)
+                                ?old[key]=chang[key]
+                                :(
+                                    old[key]===chang[key]
+                                        ?""
+                                        :old[key]=chang[key]
+                                );
+                        });
+                        return old;
+                    };
+                    const  newData=generate(dataChanged,oldData);
+
+                    // 把更新过的对象放回数据库
+                    const  requestUpdate = result.objectStore.put(newData);
+                    requestUpdate.onerror = function(event) {
+                        resolve({data:`更改失败:${event.target.error.message}`,result:false});
+                    };
+                    requestUpdate.onsuccess = function(event) {
+                        resolve({result:event.type==="success"});
+                    };
+                };
+            })
+        })
+    },
 };
